0facf8cb518234688e2d910c1ad7c09f6c8b4f32
my-eunjin <ej3133@naver.com>
2025-08-06 15:03:03 +0900

    project inital setup

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'',
+  base:'',  // GitHub 레포지토리 이름에 맞게 설정
   publicPath:'/weather/',
   build:{
-    outDir:'dist',
+    outDir:'dist',  // 빌드 출력 폴더 설정 (기본적으로 'dist')
+    rollupOptions: {
+      input: 'index.html',  // index.html이 빌드에 포함되도록 설정
+    },
   }
 })
