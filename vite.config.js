8072f6233d1b1e40dd70a8bb19bdb90dd008e520
my-eunjin <ej3133@naver.com>
2025-08-06 14:56:42 +0900

    project inital setup

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base:'',
-  publicPath:'/weather/'
+  publicPath:'/weather/',
+  build:{
+    outDir:'dist',
+  }
 })
